e124321b0bc42836c19d86c5cfb6d9b43a2779af
Nikolay <74739572+budnikme@users.noreply.github.com>
2022-05-16 21:44:50 +0200

    add EventsController

---
+++ tickets/Controllers/EventsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tickets.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+    private readonly ILogger<EventsController> _logger;
+
+    public EventsController(ILogger<EventsController> logger)
+    {
+        _logger = logger;
+    }
+
+    
+}
