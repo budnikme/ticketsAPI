12453778ac1bbfea35dd7677c9d26141a6ef6f47
Nikolay <74739572+budnikme@users.noreply.github.com>
2022-05-16 21:45:38 +0200

    add TicketsController

---
+++ tickets/Controllers/TicketsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tickets.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TicketsController : ControllerBase
+{
+    private readonly ILogger<TicketsController> _logger;
+
+    public TicketsController(ILogger<TicketsController> logger)
+    {
+        _logger = logger;
+    }
+
+    
+}
