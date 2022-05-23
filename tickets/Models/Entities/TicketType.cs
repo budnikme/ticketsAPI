307a4939208c12decc2cbe01652cc9dc462d4e36
Nikolay <74739572+budnikme@users.noreply.github.com>
2022-05-23 12:25:48 +0200

    add tickets count

--- tickets/Models/Entities/TicketType.cs
+++ tickets/Models/Entities/TicketType.cs
@@ -3,11 +3,12 @@
     public partial class TicketType
     {
         public int Id { get; set; }
-        public int? EventId { get; set; }
+        public int EventId { get; set; }
         public string? Tittle { get; set; }
         public string? Description { get; set; }
         public decimal? Price { get; set; }
+        public int? Count { get; set; }
 
-        public virtual Event? Event { get; set; }
+        public virtual Event Event { get; set; } = null!;
     }
 }
