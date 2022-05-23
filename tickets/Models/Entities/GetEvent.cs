71e1a57ef45be8737ecc1b7910a6ebe90f728ce7
Nikolay <74739572+budnikme@users.noreply.github.com>
2022-05-23 12:26:32 +0200

    add getEvent entity

---
+++ tickets/Models/Entities/GetEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tickets.Models.Entities
+{
+    public partial class GetEvent
+    {
+        public string? Tittle { get; set; }
+        public decimal? Price { get; set; }
+        public string? City { get; set; }
+        public string? Place { get; set; }
+        public DateTime? Date { get; set; }
+        public string? PreviewLink { get; set; }
+        public string? PosterLink { get; set; }
+    }
+}
