cce4d2cdc21cd29d3cd3b7429991702c7bbea017
Nikolay <74739572+budnikme@users.noreply.github.com>
2022-06-07 00:14:13 +0200

    Add ticket type id to response

--- tickets/Dto/TicketTypeDto.cs
+++ tickets/Dto/TicketTypeDto.cs
@@ -2,6 +2,7 @@
 
 public class TicketTypeDto
 {
+    public int Id { get; set; }
     public string? Tittle { get; set; }
     public string? Description { get; set; }
     public decimal? Price { get; set; }
